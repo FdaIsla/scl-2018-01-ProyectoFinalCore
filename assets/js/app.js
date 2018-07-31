8cbd878e09e953e590a7a18c016031ebe2e99b70
Sofia Maturana M <sofimatsu@gmail.com>
2018-07-31 16:16:33 -0400

    firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,47 @@
-// function loginWithFirebase() {
-//     const emailValue = userEmail.value;
-//     const passwordValue = userPassword.value;
+function loginWithFirebase() {
+    const emailValue = userEmail.value;
+    const passwordValue = userPassword.value;
 
 
-//     firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
-//         .then(() => {
-//             console.log("Usuario inició sesión con éxito");
-//             alert("¡Has iniciado sesión con éxito, bienvenid@!");
-//             redirectFromLogin()
-//         })
-//         .catch((error) => {
-//             console.log("Error de firebase > Código > " + error.code); //error.code nos mostrará el código de error para informarnos qué pasó
-//             alert("Error de firebase > Código > " + error.code);
-//             console.log("Error de firebase > Mensaje > " + error.message); //error.message nos mostrará el mensaje de firebase del mismo error
-//             alert("Error de firebase > Mensaje > " + error.message);
-//         });
-// }
-// //  validación de registro
-// function isValidEmailFormat(userInput) {
-//     if (userInput.includes("@")) {
-//         if (substr(userInput.indexOf("@")).includes(".")) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     } else {
-//         return false;
-//     }
-// }
+    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+        .then(() => {
+            console.log("Usuario inició sesión con éxito");
+            alert("¡Has iniciado sesión con éxito, bienvenid@!");
+            redirectFromLogin()
+        })
+        .catch((error) => {
+            console.log("Error de firebase > Código > " + error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            alert("Error de firebase > Código > " + error.code);
+            console.log("Error de firebase > Mensaje > " + error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+            alert("Error de firebase > Mensaje > " + error.message);
+        });
+}
+//  validación de registro
+function isValidEmailFormat(userInput) {
+    if (userInput.includes("@")) {
+        if (substr(userInput.indexOf("@")).includes(".")) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
 
-// function isValidPasswordFormat(userInput) {
-//     if (userInput.lenght <= 8 && isInteger(userInput)) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
+function isValidPasswordFormat(userInput) {
+    if (userInput.lenght <= 8 && isInteger(userInput)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// function isInteger(userInput) {
-//     let inputIsANumber = parseInt(userInput) != NaN;
-//     let inputIsAnInteger = userInput % 1 === 0;
-//     return inputIsANumber && inputIsAnInteger;
-// }
+function isInteger(userInput) {
+    let inputIsANumber = parseInt(userInput) != NaN;
+    let inputIsAnInteger = userInput % 1 === 0;
+    return inputIsANumber && inputIsAnInteger;
+}
 
 // Usuarios con sesión activa
 // firebase.auth().onAuthStateChanged(function(user) {
