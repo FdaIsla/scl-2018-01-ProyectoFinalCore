5f3722f0673656877c2057956d43b27640c2dc3d
Sofia Maturana M <sofimatsu@gmail.com>
2018-07-31 21:14:00 -0400

    cambios register

--- assets/js/appRegister.js
+++ assets/js/appRegister.js
@@ -1,38 +1,63 @@
-function visitRegister() {
+// function visitRegister() {
 
-    let nombre = name.value;
-    let rut = rutNumber.value;
-    let telefono = phone.value;
-    let hora = datetime;
-    console.log(rut)
-        // Para obtener la fecha actual
-    Date.prototype.today = function() {
-        return ((this.getDate() < 10) ? "0" : "") + this.getDate() + "/" + (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1) + "/" + this.getFullYear();
+//     let nombre = name.value;
+//     let rut = rutNumber.value;
+//     let telefono = phone.value;
+//     let hora = datetime;
+//     console.log(rut)
+//         // Para obtener la fecha actual
+//     Date.prototype.today = function() {
+//         return ((this.getDate() < 10) ? "0" : "") + this.getDate() + "/" + (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1) + "/" + this.getFullYear();
+//     }
+
+//     // Para obtener la hora/min/sec actual
+//     Date.prototype.timeNow = function() {
+//         return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
+//     }
+
+//     var datetime = "LastSync: " + new Date().today() + " @ " + new Date().timeNow();
+
+//     // const firebase = require("firebase");
+//     // // Required for side-effects
+//     // require("firebase/firestore");
+
+
+//     db.collection("Visitantes").add({
+//             Nombre: nombre,
+//             Rut: rut,
+//             Teléfono: telefono,
+//             Hora: hora,
+//         })
+//         .then(function(docRef) {
+//             console.log("Document written with ID: ", docRef.id);
+//         })
+//         .catch(function(error) {
+//             console.error("Error adding document: ", error);
+//         });
+
+// }
+
+$(document).ready(function() {
+    $('select').formSelect();
+});
+
+validation = () => {
+    let nombre = document.getElementById('name').value
+    let rut = document.getElementById('rutNumber').value
+    let telefono = document.getElementById('phone').value
+    let destination = document.getElementById('destination').value
+    let time = new Date().toLocaleString()
+    if (nombre === '' || rut == '' || destination == '' || telefono == '') {
+        console.log('complete')
+        status.style.backgroundColor = '#f9dae3';
+        status.innerHTML = `<span class="status">Complete los cambios requeridos</span>`
+    } else {
+        status.style.backgroundColor = '#c3e1c3';
+        status.innerHTML = `<span class="status">Agregado</span>`
+        redirectFromRegister()
     }
+};
 
-    // Para obtener la hora/min/sec actual
-    Date.prototype.timeNow = function() {
-        return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
-    }
-
-    var datetime = "LastSync: " + new Date().today() + " @ " + new Date().timeNow();
-
-    // const firebase = require("firebase");
-    // // Required for side-effects
-    // require("firebase/firestore");
-
-
-    db.collection("Visitantes").add({
-            Nombre: nombre,
-            Rut: rut,
-            Teléfono: telefono,
-            Hora: hora,
-        })
-        .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });
-
+function redirectToRegister() {
+    location.href = "registro.html"
 }
